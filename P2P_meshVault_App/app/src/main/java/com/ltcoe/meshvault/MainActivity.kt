e0f03f038bea313329b3f06e6df595c5fd8e5e2c
Normie69K <157805964+Normie69K@users.noreply.github.com>
2026-02-17 01:16:11 +0530

    Added application Structure

---
+++ P2P_meshVault_App/app/src/main/java/com/ltcoe/meshvault/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ltcoe.meshvault
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ltcoe.meshvault.ui.theme.P2P_meshVaultTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            P2P_meshVaultTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    P2P_meshVaultTheme {
+        Greeting("Android")
+    }
+}
---
+++ P2P_meshVault_App/app/src/main/java/com/ltcoe/meshvault/ui/theme/Shape.kt
@@ -0,0 +1,2 @@
+package com.ltcoe.meshvault.ui.theme
+
