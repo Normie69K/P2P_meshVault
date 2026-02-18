9b40451feed2ac72bb87570ce2fd8aeaa6e0c595
Normie69K <157805964+Normie69K@users.noreply.github.com>
2026-02-18 23:32:34 +0530

    added dependecies

--- P2P_meshVault_backend/build.gradle.kts
+++ P2P_meshVault_backend/build.gradle.kts
@@ -3,6 +3,7 @@ val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "2.3.0"
+    kotlin("plugin.serialization") version "2.3.0"
     id("io.ktor.plugin") version "3.4.0"
 }
 
@@ -20,7 +21,10 @@ kotlin {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-call-logging")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host")
