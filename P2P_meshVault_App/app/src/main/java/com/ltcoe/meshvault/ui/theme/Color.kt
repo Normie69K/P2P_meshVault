ca4e4804e02552d3d05893d48123968af05c6d69
Normie69K <157805964+Normie69K@users.noreply.github.com>
2026-03-03 02:25:58 +0530

    Setup project theme and dependency

--- P2P_meshVault_App/app/src/main/java/com/ltcoe/meshvault/ui/theme/Color.kt
+++ P2P_meshVault_App/app/src/main/java/com/ltcoe/meshvault/ui/theme/Color.kt
@@ -2,7 +2,10 @@ package com.ltcoe.meshvault.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val DarkBackground = Color(0xFF0F141E) // Deep navy/black
+val SurfaceDark = Color(0xFF1A1F2B)    // Slightly lighter for cards
+val AccentCyan = Color(0xFF10CBEA)     // The neon button color val Purple80 = Color(0xFFD0BCFF)
+val SuccessGreen = Color(0xFF00E676) // Neon green for status indicators
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
